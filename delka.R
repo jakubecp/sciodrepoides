4cf47609aa84176455e12bfac6c8bac7ee0be261
Pavel <a.headache@seznam.cz>
2015-08-17 15:25:48 +0200

    added space to labels

--- delka.R
+++ delka.R
@@ -22,8 +22,8 @@ tiff (filename="exports/sciodrepoides_length.tiff", width=5000, height=6000,
       compression="lzw", res= 800)
 qplot (data$instar,data$delka,
        geom="boxplot",
-      xlab=substitute(Instar),
-      ylab=substitute (Length (mm)))
+      xlab=substitute("Instar"),
+      ylab=substitute ("Length (mm)"))
 dev.off()
 
 
